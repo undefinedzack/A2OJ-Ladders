6989367ce97b1bdae0092b1b723427412390a89d
Adhney <adhney.nawghare@gmail.com>
2021-07-29 14:46:20 +0530

    minor tweaks

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,122 +1,180 @@
-import {useState, useEffect, useRef} from 'react'
-import axios from 'axios'
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-import {ladders} from './data'
+import { ladders } from "./data";
 
 const Home = () => {
+  const [username, setUsername] = useState("undefinedzack");
+  const [details, setDetails] = useState();
+  const [division, setDivision] = useState(ladders.div_a);
 
+  const [flag, setFlag] = useState(false);
 
-    const [username, setUsername] = useState('undefinedzack')
-    const [details, setDetails] = useState()
-    const [division, setDivision] = useState(ladders.div_a)
+  const count = useRef(0);
 
-    const [flag, setFlag] = useState(false)
+  const arr = [];
 
-    const count = useRef(0)
+  useEffect(() => {
+    const fetchDetails = () => {
+      const url = "https://codeforces.com/api/user.status?handle=" + username;
+      axios.get(url).then((res) => setDetails(res));
+    };
 
-    const arr = []
+    fetchDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [flag]);
 
-    useEffect(() => {
-        const fetchDetails = () => {
-            const url = 'https://codeforces.com/api/user.status?handle=' + username
-            axios.get(url)
-                .then(res => setDetails(res))
-        }
+  return (
+    <>
+      {/*{details && console.log(details)}*/}
+      <div className="container mt-5 mb-5">
+        <div className="d-flex justify-content-center">
+          <h1>A2OJ</h1>
+        </div>
+        {/* <div className='d-flex justify-content-center'>
+            <p style={{fontSize: '25px'}}>Solved - {count.current}</p>
+            <hr/>
+        </div> */}
 
-        fetchDetails()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [flag])
+        <div className="input-group mb-5 mt-3 shadow bg-body rounded">
+          <span className="input-group-text" id="basic-addon1">
+            @
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button
+            className="btn btn-info"
+            type="button"
+            onClick={() => setFlag(!flag)}
+          >
+            Go!
+          </button>
+        </div>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <button
+              className="nav-link"
+              onClick={() => setDivision(ladders.div_a)}
+            >
+              Div A
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className="nav-link"
+              onClick={() => setDivision(ladders.div_b)}
+            >
+              Div B
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className="nav-link"
+              onClick={() => setDivision(ladders.div_c)}
+            >
+              Div C
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className="nav-link"
+              onClick={() => setDivision(ladders.div_d)}
+            >
+              Div D
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className="nav-link"
+              onClick={() => setDivision(ladders.div_e)}
+            >
+              Div E
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className="nav-link"
+              onClick={() => setDivision(ladders.div_1d)}
+            >
+              Div F
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className="nav-link"
+              onClick={() => setDivision(ladders.div_1e)}
+            >
+              Div G
+            </button>
+          </li>
+        </ul>
 
-    return (
-        <>
-            {/*{details && console.log(details)}*/}
-            <div className='container mt-5 mb-5'>
-                <div className='d-flex justify-content-center'>
-                    <h1>A2OJ</h1>
-                </div>
-                <div className='d-flex justify-content-center'>
-                    <p style={{fontSize: '25px'}}>Solved - {count.current}</p>
-                    <hr/>
-                </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">{(count.current = 0)}</th>
+              <th scope="col">Problem</th>
+              <th className="d-flex justify-content-center" scope="col">
+                Solved!
+              </th>
+            </tr>
+          </thead>
 
-                <div className="input-group mb-5 mt-3 shadow bg-body rounded">
-                    <span className="input-group-text" id="basic-addon1">@</span>
-                    <input type="text" className="form-control" placeholder="Username"
-                           aria-label="Username" aria-describedby="basic-addon1"
-                           value={username} onChange={(e) => setUsername(e.target.value)}/>
-                    <button className="btn btn-info" type="button" onClick={() => setFlag(!flag)}>Go!</button>
-                </div>
-                <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={() => setDivision(ladders.div_a)}>Div A</button>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={() => setDivision(ladders.div_b)}>Div B</button>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={() => setDivision(ladders.div_c)}>Div C</button>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={() => setDivision(ladders.div_d)}>Div D</button>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={() => setDivision(ladders.div_e)}>Div E</button>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={() => setDivision(ladders.div_1d)}>Div F</button>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={() => setDivision(ladders.div_1e)}>Div G</button>
-                    </li>
-                </ul>
+          <tbody>
+            {division.map((prob, index) => {
+              return (
+                <tr key={index}>
+                  <th scope="row">{prob[0]}</th>
+                  <td>
+                    <a
+                      href={
+                        "https://codeforces.com/problemset/problem/" +
+                        prob[2] +
+                        "/" +
+                        prob[3]
+                      }
+                    >
+                      {" "}
+                      {prob[1]}{" "}
+                    </a>
+                  </td>
 
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">{count.current = 0}</th>
-                        <th scope="col">Problem</th>
-                        <th className='d-flex justify-content-center' scope="col">Solved!</th>
-                    </tr>
-                    </thead>
-
-                    <tbody>
-
-                    {
-                        division.map((prob, index) => {
+                  {details &&
+                    details.data.result.map((item) => {
+                      if (
+                        item.problem.contestId.toString() === prob[2] &&
+                        item.problem.index.toString() === prob[3] &&
+                        item.verdict === "OK" &&
+                        !arr.includes(item.problem.contestId.toString())
+                      ) {
+                        arr.push(item.problem.contestId.toString());
+                        count.current += 1;
                         return (
-                            <tr key={index}>
-                                <th scope="row">{prob[0]}</th>
-                                <td><a
-                                    href={'https://codeforces.com/problemset/problem/' + prob[2] + '/' + prob[3]}> {prob[1]} </a>
-                                </td>
-
-                                {details && details.data.result.map((item,) => {
-
-                                    if (item.problem.contestId.toString() === prob[2]
-                                        && item.problem.index.toString() === prob[3]
-                                        && item.verdict === 'OK'
-                                        && !arr.includes(item.problem.contestId.toString())
-                                    ) {
-                                        arr.push(item.problem.contestId.toString())
-                                        count.current += 1
-                                        return (
-                                            <td className='table-success'>
-                                                {/*{console.log('arr',arr)}*/}
-                                                <div className='d-flex justify-content-center'><i
-                                                    className="bi bi-check2"/></div>
-                                            </td>
-                                        )
-                                    }
-                                    return <></>
-                                })}
-                            </tr>
-                        )
+                          <td className="table-success">
+                            {/*{console.log('arr',arr)}*/}
+                            <div className="d-flex justify-content-center">
+                              <i className="bi bi-check2" />
+                            </div>
+                          </td>
+                        );
+                      }
+                      return <></>;
                     })}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
-}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
